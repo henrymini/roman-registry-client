6ee6716221e50324daac493498b4428342b5e321
RWPicklechips <hminicucci@gmail.com>
2019-12-16 14:43:48 -0500

    Second polish  commit
    added user facing messages for create, updte and delete

--- assets/scripts/generals/ui.js
+++ assets/scripts/generals/ui.js
@@ -30,24 +30,36 @@ const onShowSuccess = responseData => {
 
 const onDeleteSuccess = () => {
   $('#general-display').val('')
-  $('#message').val('successful removed')
+  onSuccess('SUCCESSFULLY REMOVED')
   $('form').trigger('reset')
 }
 
 const onUpdateSuccess = () => {
-  $('#general-display').val('successful updated')
+  $('#general-display').val('')
+  onSuccess('SUCCESSFULLY UPDATED')
   $('form').trigger('reset')
 }
 
 const onCreateSuccess = () => {
-  $('#general-display').val('successful created')
+  $('#general-display').val('')
+  onSuccess('SUCCESSFULLY CREATED')
   $('form').trigger('reset')
 }
 
-const onFailure = error => {
-  // console.log(error)
-  $('#general-display').val('failure')
-  $('form').trigger('reset')
+const onSuccess = message => {
+  $('#message')
+    .removeClass('failure')
+    .addClass('success')
+    .text(message)
+  $('input').val('')
+}
+
+const onFailure = message => {
+  $('#message')
+    .removeClass('success')
+    .addClass('failure')
+    .text('something went wrong')
+  $('input').val('')
 }
 
 module.exports = {
